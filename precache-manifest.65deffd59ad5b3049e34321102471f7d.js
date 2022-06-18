0a10f925982ce21036066542ba51069894d08c5b
jiangrenjie <jiangrenjie@hhu.edu.cn>
2022-06-18 10:13:43 +0800

    deploy

---
+++ precache-manifest.65deffd59ad5b3049e34321102471f7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e8e13e56379eda0447",
+    "url": "/lucky-cat-website/css/app.48ebdcc2.css"
+  },
+  {
+    "revision": "384bdd7df8fe5bf18f08a6fb8cec350e",
+    "url": "/lucky-cat-website/index.html"
+  },
+  {
+    "revision": "e2e8e13e56379eda0447",
+    "url": "/lucky-cat-website/js/app.8b8a2ae9.js"
+  },
+  {
+    "revision": "1ab36113775e9436a0b4",
+    "url": "/lucky-cat-website/js/chunk-vendors.76f043a7.js"
+  },
+  {
+    "revision": "4f29e115fb21aa714e8aeabe626a6fbd",
+    "url": "/lucky-cat-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/lucky-cat-website/robots.txt"
+  }
+]);
